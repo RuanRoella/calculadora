8e0df2539acb6a5aca5d920e81f21c3bbd2e7bcc
RuanRoella <ruan_shull@hotmail.com>
2024-06-07 21:22:11 -0300

    Implementação de funcionalidade da calculadora

--- app/calculator.py
+++ app/calculator.py
@@ -1,49 +1,143 @@
 import tkinter as tk
-import json
 
 from .core import KEYBOARD
+from .core import Settings
+from .core import Calculator
 
 class Application(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
-        self.set = self.load_json('setup')
-        self.setup(self.set['title'], self.set['width'], self.set['height'])
+        self.calculate = Calculator()
+        self.set = Settings()
+        self.setup(self.set.main.title, self.set.main.width, self.set.main.height)
 
-        main_frame = tk.Frame(bg='#ffffff')
+        main_frame = tk.Frame(bg=self.set.main.background)
         main_frame.pack(side=tk.TOP, expand=True, fill=tk.BOTH)
 
-        self.value = tk.StringVar()
-        self.value.set(0)
+        self.prev_var = tk.StringVar()
+        self.preview = tk.Entry(main_frame, textvariable=self.prev_var, cnf=self.set.preview)
+        self.preview.pack(side=tk.TOP, fill=tk.X, padx=2, pady=(2, 0), ipady=2)
 
-        self.display = tk.Entry(main_frame, textvariable=self.value, readonlybackground="#ffffff", font=('Console', 24, 'bold'), highlightthickness=0, relief=tk.FLAT, justify=tk.RIGHT, state='readonly')
-        self.display.pack(side=tk.TOP, padx=5, pady=(10,0), ipady=5)
-    
-        buttons = tk.Frame(main_frame, bg="#ffffff")
-        buttons.pack(side=tk.BOTTOM, expand=True, fill=tk.BOTH, padx=5, pady=(0, 10))
- 
+        self.display = tk.Entry(main_frame, cnf=self.set.display)
+        self.display.insert(0,0)
+        self.display.pack(side=tk.TOP, padx=2, pady=0, ipady=2)
+
+        buttons = tk.Frame(main_frame, bg=self.set.main.background)
+        buttons.pack(side=tk.BOTTOM, expand=True, fill=tk.BOTH, padx=2, pady=(0, 2))
+
         self.btns = {}
         for r in range(len(KEYBOARD)):
             buttons.rowconfigure(r+1, weight=1)
             for c in range(len(KEYBOARD[r])):
                 buttons.columnconfigure(c, weight=1)
 
-                self.btns[f'btn_{str(KEYBOARD[r][c])}'] = tk.Button(buttons, text=str(KEYBOARD[r][c]), bd=0, highlightthickness=0, bg="#f8f8f2", fg="#000000", font=('Cambria', 10, 'bold'))
+                self.btns[f'btn_{str(KEYBOARD[r][c])}'] = tk.Button(buttons, text=str(KEYBOARD[r][c]), cnf=self.set.button)
+                self.btns[f'btn_{str(KEYBOARD[r][c])}'].bind('<ButtonPress>', self.button_command)
                 self.btns[f'btn_{str(KEYBOARD[r][c])}'].grid(row=r+1, column=c, padx=(1, 0), pady=(1, 0), sticky=tk.NSEW)
 
-        self.btns['btn_='].config(bg="#47b2f5", fg="#000000")
+        self.btns['btn_='].config(cnf=self.set.button_equal)
 
-    @staticmethod
-    def load_json(optional: str):
-        with open('app/settings.json', 'r', encoding='utf-8') as file:
-            json_file = json.load(file)
 
-        return json_file[optional]
+    def button_command(self, e):
+
+        button = e.widget.cget('text')
+
+        if button in [str(i) for i in range(10)]:
+            self.display_numeric(int(button))
+        elif button == "\u232B":
+            self.set_backspace()
+        elif button in ["\u00F7", 'x', '-', '+']:
+            self.set_operators(button)
+        elif button in ["CE", "C"]:
+            self.clean_data(button)
+        elif button in ["¹/\u02E3", "\u02E3²", "²\u221A\u02E3"]:
+            result = self.calculate.get_x_element(self.display.get(), button)
+            self.display.delete(0, tk.END)
+            self.display.insert(0, result['display'])
+            self.prev_var.set(result['preview'])
+        elif button == "%":
+            result = self.calculate.get_porcent(self.prev_var.get())
+            self.display.delete(0, tk.END)
+            self.display.insert(0, result['display'])
+            self.prev_var.set(result['preview'])
+        elif button == ",":
+            ...
+        elif button == "←":
+            ...
+        else:
+            result = self.calculate.eval(self.prev_var.get())
+            self.display.delete(0, tk.END)
+            self.display.insert(0, result)
+            self.prev_var.set("%s%s" % (self.prev_var.get(), '='))
+        
+            
+    def display_numeric(self, value: int):
+        if len(self.display.get()) >= 13:
+            return
+
+        if self.display.get() == "0":
+            self.display.delete(0)
+            self.display.insert(0, str(value))
+        else:
+            self.display.insert(tk.END, str(value))
+
+        express = "%s%s" % (self.prev_var.get(), self.display.get()[-1])
+        if value == 0 and len(self.display.get()) <= 1:
+            self.display.insert(tk.END, ',')
+            express = "%s%s" % (self.prev_var.get(), self.display.get())
+        
+        self.prev_var.set(express)
+
+    def set_backspace(self):
+
+        if self.display.get() == "0":
+            return
+        
+        if len(self.display.get()) <= 1:
+            self.display.delete(0, tk.END)
+            self.prev_var.set("")
+
+            self.display.insert(tk.END, 0)
+        else:
+            self.display.delete(len(self.display.get())-1, tk.END)
+            self.prev_var.set(self.display.get())
+
+    def set_operators(self, operator: str):
+        
+        if self.display.get() == "0" or self.prev_var.get()[-1] == operator:
+            return
+        
+        if self.prev_var.get()[-1] == "=":
+            self.prev_var.set("%s%s" % (self.display.get(), operator))
+
+        self.display.delete(0, tk.END)
+        self.display.insert(tk.END, 0)
+
+        if self.prev_var.get()[-1] in ["\u00F7", 'x', '-', '+']:
+            new_express = self.prev_var.get()[:-1]
+            self.prev_var.set(new_express)
+        
+        express = "%s%s" % (self.prev_var.get(), operator)
+        self.prev_var.set(express)
     
+    def clean_data(self, btn):
+
+        if btn == "CE":
+            index = self.prev_var.get().find(self.display.get())
+            if index != -1:
+                self.prev_var.set(self.prev_var.get()[:index])
+            self.display.delete(0, tk.END)
+            self.display.insert(tk.END, 0)
+        else:
+            self.display.delete(0, tk.END)
+            self.display.insert(tk.END, 0)
+            self.prev_var.set("")
+
     def setup(self, title: str, width: int, height: int):
         self.wm_title(title)
 
-        width = width
-        height = height
+        width = int(width)
+        height = int(height)
 
         w_screen = self.winfo_screenwidth()
         h_screen = self.winfo_screenheight()
--- app/core.py
+++ app/core.py
@@ -1,7 +1,119 @@
+from configparser import ConfigParser
+from pathlib import Path
+
+import re
 
 KEYBOARD = [["%", "CE", "C", "\u232B"],
             ["¹/\u02E3", "\u02E3²", "²\u221A\u02E3", "\u00F7"],
-            ["7", "8", "9", 'X'],
+            ["7", "8", "9", 'x'],
             ["4", "5", "6", "-"],
             ["1", "2", "3", "+"],
             ["←", "0", ",", "="]]
+
+
+
+class Calculator:
+    
+    def get_x_element(self, value: int, expression: str):
+
+        if expression:
+            
+            calc = None
+            preview = None
+            match expression:
+                case "¹/\u02E3": # 1/x
+                    calc = 1 / float(value)
+                    preview = "1/(%s)" % value
+                case "\u02E3²": # x²
+                    calc = float(value) ** 2
+                    preview = "sqr(%s)" % value
+                case "²\u221A\u02E3": # Raiz
+                    calc = float(value) ** (1/2)
+                    preview = "\u221A(%s)" % value
+
+            if calc.is_integer():
+                return {"display": int(calc), 'preview': preview}
+            
+            point = len(str(calc).split('.')[-1] )
+            result = "%.{0}f".format(point if point <= 14 else 14) % calc
+            
+            return {"display": result.replace('.', ','), 'preview': preview}
+
+    #TODO Semantic Error
+    #Fix somes result is returning wrong values
+    def get_porcent(self, calc):
+        
+        _all = calc[:1]
+        _part = calc[-1]
+        
+        if _all.isnumeric():
+            # % = (Parte / Todo) x 100
+            porc = (float(_part) * float(_all)) / 100
+
+        new_expression = str(porc).replace('.', ',')
+
+        return {"display": new_expression, "preview": "%s%s" % (calc[:-1], porc)}
+        
+    def eval(self, calc):
+        
+        try:
+            value = self._normalizate_expression(calc)
+        except ZeroDivisionError:
+            value = "Error"
+        finally:
+            return value
+        
+
+    def _normalizate_expression(self, expression: str):
+        
+        operators = ['+', '-', 'x', '÷']
+
+        exp = re.compile('|'.join('(?:{})'.format(re.escape(i)) for i in sorted(operators, reverse=True, key=len)))
+        
+        _split_exp = ""
+        _new_exp = ""
+        find_op = exp.findall(expression)
+        if len(find_op) > 0:
+            for op in find_op:
+                _split_exp = expression.split(op)
+                if op == "÷":
+                    _op = "/"
+                elif op == "x":
+                    _op = "*"
+                else:
+                    _op = op
+                _new_exp = "%s%s%s" % (_split_exp[0], _op, _split_exp[1])
+        
+        
+        result = eval(_new_exp)
+        point = len(str(result).split('.')[-1] )
+
+        if isinstance(result, int):
+            return result
+        
+        value = "%.{0}f".format(point if point <= 14 else 14) % result
+        return value.replace('.', ',')
+
+
+
+class Dict(dict):
+    def __init__(self, *args, **kwargs):
+        super(Dict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
+    
+    def __getattribute__(self, name: str):
+        try:
+            if isinstance(name, str):
+                return super().__getattribute__(name)
+        except AttributeError:
+            pass
+
+class Settings:
+    data = Path(r'app\DATA.ini')
+
+    config = ConfigParser(dict_type=Dict)
+
+    config.read(data)
+
+    def __new__(cls):
+        return cls.config._sections
