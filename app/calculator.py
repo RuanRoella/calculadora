d83bea364bb5cea9e1246429cb77bc18a52572b9
RuanRoella <ruan_shull@hotmail.com>
2024-06-09 00:21:08 -0300

    Correções na funcionalidade da calculadora

--- app/calculator.py
+++ app/calculator.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-
+import re
 from .core import KEYBOARD
 from .core import Settings
 from .core import Calculator
@@ -9,7 +9,8 @@ class Application(tk.Tk):
         tk.Tk.__init__(self)
         self.calculate = Calculator()
         self.set = Settings()
-        self.setup(self.set.main.title, self.set.main.width, self.set.main.height)
+        self.setup()
+        self.key_up = []
 
         main_frame = tk.Frame(bg=self.set.main.background)
         main_frame.pack(side=tk.TOP, expand=True, fill=tk.BOTH)
@@ -36,57 +37,59 @@ class Application(tk.Tk):
                 self.btns[f'btn_{str(KEYBOARD[r][c])}'].grid(row=r+1, column=c, padx=(1, 0), pady=(1, 0), sticky=tk.NSEW)
 
         self.btns['btn_='].config(cnf=self.set.button_equal)
+        # Undefined Button
+        self.btns['btn_←'].configure(state='disabled', background="#dddddd")
 
 
     def button_command(self, e):
 
         button = e.widget.cget('text')
 
-        if button in [str(i) for i in range(10)]:
-            self.display_numeric(int(button))
-        elif button == "\u232B":
+        self.key_up.append(button)
+
+        last_key = self.key_up[-1]
+        
+        if last_key in ["\u00F7", 'x', '-', '+']:
+            self.set_operators(last_key)
+        elif last_key in [str(i) for i in range(10)]:
+            self.display_numeric(last_key)
+        elif last_key == ",":
+            self.set_float_point()
+        elif last_key in ['CE', 'C']:
+            self.clean_data(last_key)
+        elif last_key == "\u232B":
             self.set_backspace()
-        elif button in ["\u00F7", 'x', '-', '+']:
-            self.set_operators(button)
-        elif button in ["CE", "C"]:
-            self.clean_data(button)
-        elif button in ["¹/\u02E3", "\u02E3²", "²\u221A\u02E3"]:
-            result = self.calculate.get_x_element(self.display.get(), button)
+        elif last_key in ["¹/\u02E3", "\u02E3²", "²\u221A\u02E3"]:
+            result = self.calculate.get_x_element(self.display.get(), last_key)
             self.display.delete(0, tk.END)
             self.display.insert(0, result['display'])
             self.prev_var.set(result['preview'])
-        elif button == "%":
-            result = self.calculate.get_porcent(self.prev_var.get())
+        elif last_key == "%":
+            result = self.calculate.get_porcent(self.display.get(), self.prev_var.get())
             self.display.delete(0, tk.END)
             self.display.insert(0, result['display'])
             self.prev_var.set(result['preview'])
-        elif button == ",":
-            ...
-        elif button == "←":
-            ...
-        else:
-            result = self.calculate.eval(self.prev_var.get())
+        elif last_key == "=":
+            result = self.calculate.eval(self.display.get(), self.prev_var.get())
             self.display.delete(0, tk.END)
             self.display.insert(0, result)
-            self.prev_var.set("%s%s" % (self.prev_var.get(), '='))
-        
-            
+            self.prev_var.set("%s%s =" % (self.prev_var.get(), self.display.get()))
+
+
     def display_numeric(self, value: int):
-        if len(self.display.get()) >= 13:
+
+        if len(self.display.get()) > 14:
             return
 
         if self.display.get() == "0":
             self.display.delete(0)
-            self.display.insert(0, str(value))
-        else:
-            self.display.insert(tk.END, str(value))
-
-        express = "%s%s" % (self.prev_var.get(), self.display.get()[-1])
-        if value == 0 and len(self.display.get()) <= 1:
-            self.display.insert(tk.END, ',')
-            express = "%s%s" % (self.prev_var.get(), self.display.get())
+       
+        if len(self.key_up) > 1:
+            
+            if self.key_up[-2] in ["\u00F7", 'x', '-', '+']:
+                self.display.delete(0, tk.END)
         
-        self.prev_var.set(express)
+        self.display.insert(tk.END, value)
 
     def set_backspace(self):
 
@@ -104,40 +107,59 @@ class Application(tk.Tk):
 
     def set_operators(self, operator: str):
         
-        if self.display.get() == "0" or self.prev_var.get()[-1] == operator:
+        if self.display.get() == "0":
             return
-        
-        if self.prev_var.get()[-1] == "=":
-            self.prev_var.set("%s%s" % (self.display.get(), operator))
 
-        self.display.delete(0, tk.END)
-        self.display.insert(tk.END, 0)
+        value = self.display.get()
 
-        if self.prev_var.get()[-1] in ["\u00F7", 'x', '-', '+']:
-            new_express = self.prev_var.get()[:-1]
-            self.prev_var.set(new_express)
-        
-        express = "%s%s" % (self.prev_var.get(), operator)
-        self.prev_var.set(express)
-    
-    def clean_data(self, btn):
-
-        if btn == "CE":
-            index = self.prev_var.get().find(self.display.get())
-            if index != -1:
-                self.prev_var.set(self.prev_var.get()[:index])
+        _split_value = value.split(',')
+        if _split_value[-1] in ['', "0"]:
             self.display.delete(0, tk.END)
-            self.display.insert(tk.END, 0)
+            self.display.insert(0, _split_value[0])
+            value = _split_value[0]
+
+        self.prev_var.set("%s %s " % (value, operator))
+    
+    def set_float_point(self):
+        
+        if self.display.get().find(',') > 0:
+            return
+
+        # Last value of the display
+        value = self.display.get()[-1]
+
+        point = ","
+
+        if value == ",":
+            return
+        elif value in ["\u00F7", 'x', '-', '+']:
+            self.display.delete(0, tk.END)
+            point = "0,"
+        elif len(self.key_up) > 1:
+            if self.key_up[-2] in ["\u00F7", 'x', '-', '+']:
+                self.display.delete(0, tk.END)
+                point = "0,"
+
+        self.display.insert(tk.END, point)
+        
+    def clean_data(self, btn):
+        
+        if btn == "CE":
+            self.display.delete(0, tk.END)
+            self.display.insert(0, 0)
         else:
             self.display.delete(0, tk.END)
-            self.display.insert(tk.END, 0)
             self.prev_var.set("")
+            self.display.insert(0, 0)
 
-    def setup(self, title: str, width: int, height: int):
-        self.wm_title(title)
+    def setup(self):
+        
+        self.wm_iconbitmap(default=r'assets\calculator.ico')
+        
+        self.wm_title('Calculadora')
 
-        width = int(width)
-        height = int(height)
+        width = 300
+        height = 400
 
         w_screen = self.winfo_screenwidth()
         h_screen = self.winfo_screenheight()
--- app/core.py
+++ app/core.py
@@ -14,57 +14,74 @@ KEYBOARD = [["%", "CE", "C", "\u232B"],
 
 class Calculator:
     
-    def get_x_element(self, value: int, expression: str):
+    def get_x_element(self, value: str, expression: str):
+        
 
+        value = float(value.replace(',', '.'))
         if expression:
             
             calc = None
             preview = None
+            result = None
             match expression:
                 case "¹/\u02E3": # 1/x
-                    calc = 1 / float(value)
-                    preview = "1/(%s)" % value
+                    calc = 1 / value
+                    preview = "1/"
                 case "\u02E3²": # x²
-                    calc = float(value) ** 2
-                    preview = "sqr(%s)" % value
+                    calc = value ** 2
+                    preview = "sqr"
                 case "²\u221A\u02E3": # Raiz
-                    calc = float(value) ** (1/2)
-                    preview = "\u221A(%s)" % value
+                    calc = value ** (1/2)
+                    preview = "\u221A"
 
+            if value.is_integer():
+                prev = "%s( %s )" % (preview, str(int(value)))
+            else:
+                prev = "%s( %s )" % (preview, str(value).replace('.', ','))
+            
+            
             if calc.is_integer():
-                return {"display": int(calc), 'preview': preview}
-            
-            point = len(str(calc).split('.')[-1] )
-            result = "%.{0}f".format(point if point <= 14 else 14) % calc
-            
-            return {"display": result.replace('.', ','), 'preview': preview}
+                result = {"display": int(calc), 'preview': prev}
+            else:
+                point = len(str(calc).split('.')[-1] )
+                format = "%.{0}f".format(point if point <= 14 else 14) % calc
 
-    #TODO Semantic Error
-    #Fix somes result is returning wrong values
-    def get_porcent(self, calc):
+                result = {"display": format.replace('.', ','), 'preview': prev}
+            
+            return result
+
+    def get_porcent(self, display, preview):
         
-        _all = calc[:1]
-        _part = calc[-1]
-        
-        if _all.isnumeric():
+        result = "0"
+        new_expression = "0"
+        if preview:
+
+            operators = ['+', '-', 'x', '÷']
+
+            exp = re.compile('|'.join('(?:{})'.format(re.escape(i)) for i in sorted(operators, reverse=True, key=len)))
+            op = exp.search(preview).group()
+            
+            _all = preview.split(op)[0].strip()
+            _part = display.replace(',', '.')
+
             # % = (Parte / Todo) x 100
             porc = (float(_part) * float(_all)) / 100
 
-        new_expression = str(porc).replace('.', ',')
+            result = str(porc).replace('.', ',')
+            new_expression = "%s%s" % (preview, porc)
 
-        return {"display": new_expression, "preview": "%s%s" % (calc[:-1], porc)}
+        return { "display": result, "preview": new_expression }
         
-    def eval(self, calc):
-        
+    def eval(self, display, preview):
+
         try:
-            value = self._normalizate_expression(calc)
+            value = self._normalizate_expression(display, preview)
         except ZeroDivisionError:
             value = "Error"
         finally:
             return value
         
-
-    def _normalizate_expression(self, expression: str):
+    def _normalizate_expression(self, display: str, preview: str):
         
         operators = ['+', '-', 'x', '÷']
 
@@ -72,22 +89,21 @@ class Calculator:
         
         _split_exp = ""
         _new_exp = ""
-        find_op = exp.findall(expression)
+        find_op = exp.findall(preview)
         if len(find_op) > 0:
             for op in find_op:
-                _split_exp = expression.split(op)
+                _split_exp = preview.split(op)
                 if op == "÷":
                     _op = "/"
                 elif op == "x":
                     _op = "*"
                 else:
                     _op = op
-                _new_exp = "%s%s%s" % (_split_exp[0], _op, _split_exp[1])
-        
+                _new_exp = "%s%s%s" % (_split_exp[0].strip(), _op, display)
         
         result = eval(_new_exp)
         point = len(str(result).split('.')[-1] )
-
+#
         if isinstance(result, int):
             return result
         
@@ -107,6 +123,7 @@ class Dict(dict):
                 return super().__getattribute__(name)
         except AttributeError:
             pass
+
 
 class Settings:
     data = Path(r'app\DATA.ini')
